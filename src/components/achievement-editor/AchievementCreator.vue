39d644e4295660e8cae4f88fdc7ed14924fb183c
Mark Garcia <msgarcia@collin.edu>
2021-06-12 23:27:59 -0500

    Create achievement editor and output.

---
+++ src/components/achievement-editor/AchievementCreator.vue
@@ -0,0 +1,203 @@
+<template>
+  <form>
+    <base-header>Customize Your Achievement</base-header>
+    <brand-selector-list></brand-selector-list>
+    <section class="image-or-icons" v-if="!isXbox">
+      <ul>
+        <li
+          :class="{ active: activeTab === 'image' }"
+          @click="toggleActive('image')"
+        >
+          <font-awesome-icon :icon="['fad', 'image']"></font-awesome-icon> Image
+        </li>
+        <li
+          :class="{ active: activeTab === 'icons' }"
+          @click="toggleActive('icons')"
+        >
+          <font-awesome-icon :icon="['fad', 'icons']"></font-awesome-icon> Icons
+        </li>
+      </ul>
+      <div class="image-upload" v-if="hasImage && !isIcon">
+        <base-image-upload></base-image-upload>
+      </div>
+      <div class="icon-picker" v-else>
+        <icon-selector-list></icon-selector-list>
+      </div>
+    </section>
+    <div class="inputs">
+      <base-input label="Achievement Header"></base-input>
+      <base-input label="Achievement Text"></base-input>
+      <base-input label="Achievement Value" v-if="isXbox"></base-input>
+      <base-checkbox
+        text="Add Milestonne Tag"
+        :currentState="isMilestonne"
+        v-if="isMinnMax"
+        @change-state="updateMilestone"
+      ></base-checkbox>
+      <base-checkbox
+        text="Rare Achievement"
+        :currentState="isRare"
+        v-if="isXbox"
+        @change-state="updateRarity"
+      ></base-checkbox>
+    </div>
+    <trophy-selector-list v-if="isPS"></trophy-selector-list>
+  </form>
+</template>
+<script>
+import BrandSelectorList from "../achievement-editor/BrandSelectorList.vue";
+import TrophySelectorList from "../achievement-editor/TrophySelectorList.vue";
+import IconSelectorList from "../achievement-editor/IconSelectorList.vue";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+export default {
+  components: { BrandSelectorList, TrophySelectorList, IconSelectorList },
+  setup() {
+    const store = useStore();
+
+    const activeTab = ref("image");
+
+    const toggleActive = (tab) => {
+      activeTab.value = tab;
+      store.dispatch("updateIcons", tab === "icons");
+    };
+
+    const hasImage = computed(() => {
+      return store.getters.getBrand !== "xbox";
+    });
+
+    const isIcon = computed(() => {
+      return store.getters.getIsIcon;
+    });
+
+    const isMinnMax = computed(() => {
+      return store.getters.getBrand === "minnmax";
+    });
+
+    const isMilestonne = ref(false);
+
+    const isXbox = computed(() => {
+      return store.getters.getBrand === "xbox";
+    });
+
+    const isRare = computed(() => {
+      return store.getters.getRarity;
+    });
+
+    const isPS = computed(() => {
+      return store.getters.getBrand === "playstation";
+    });
+
+    const updateMilestone = (input) => {
+      isMilestonne.value = input;
+      store.dispatch("updateMilestone", input);
+    };
+
+    const updateRarity = (input) => {
+      store.dispatch("updateRarity", input);
+    };
+    return {
+      isIcon,
+      hasImage,
+      isMinnMax,
+      isXbox,
+      isRare,
+      isPS,
+      isMilestonne,
+      updateMilestone,
+      updateRarity,
+      activeTab,
+      toggleActive,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+form {
+  background: #ffffff;
+  border-radius: 30px;
+  @include boxshadow((-3px -3px 9px #eaeaea, 6px 6px 20px #d7d7d7));
+  @include flexbox();
+  align-items: center;
+  border-bottom: 1px solid #d7d7d7;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 1em auto 3em;
+  padding: 3em;
+  width: 40em;
+
+  h1 {
+    text-align: center;
+    width: 100%;
+  }
+
+  .image-or-icons {
+    @include flexbox();
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    @include transition(all 0.3s ease);
+    height: min-content;
+    width: 20em;
+
+    ul {
+      background: #eaeaea;
+      border-radius: 30px;
+      @include flexbox();
+      list-style: none;
+      justify-content: center;
+      margin: 0 0 1em;
+      padding: 0;
+      width: auto;
+
+      li {
+        color: #666;
+        margin: 3px;
+        padding: 0.5em;
+        width: 5em;
+      }
+
+      li.active {
+        color: $ps-black;
+        border-radius: 30px;
+        background: #ffffff;
+        box-shadow: 0px 5px 10px #d7d7d7;
+      }
+    }
+
+    .image-upload {
+      background: #f7f7f7;
+      border-radius: 30px;
+      margin-right: 1em;
+      overflow: hidden;
+      @include flexbox();
+      justify-content: center;
+      transition: all 0.3s ease;
+      height: 10em;
+      width: 10em;
+    }
+
+    .icon-picker {
+      background: #f7f7f7;
+      border-radius: 30px;
+      margin-right: 1em;
+      overflow: hidden;
+      @include flexbox();
+      justify-content: center;
+      transition: all 0.3s ease;
+      height: 15em;
+      width: 20em;
+    }
+
+    .image-upload:hover {
+      background: rgba(#007aff, 0.05);
+    }
+  }
+
+  .inputs {
+    @include flexbox();
+    flex-wrap: wrap;
+    flex: 2;
+  }
+}
+</style>
---
+++ src/components/achievement-editor/AchievementOutput.vue
@@ -0,0 +1,178 @@
+<template>
+  <section>
+    <base-achievement id="achievement" :mode="setBrand">
+      <minnmax-card v-if="setBrand === 'minnmax'">
+        <template v-slot:image
+          ><base-achievement-image shape="rounded"></base-achievement-image
+        ></template>
+        <template v-slot:text>
+          <div class="text-wrapper" :class="setBrand">
+            <base-achievement-header></base-achievement-header
+            ><base-achievement-text></base-achievement-text></div></template
+      ></minnmax-card>
+
+      <nintendo-card v-else-if="setBrand === 'nintendo'">
+        <template v-slot:image
+          ><base-achievement-image></base-achievement-image
+        ></template>
+        <template v-slot:text>
+          <div class="text-wrapper" :class="setBrand">
+            <base-achievement-header></base-achievement-header
+            ><base-achievement-text></base-achievement-text></div></template
+      ></nintendo-card>
+
+      <xbox-card v-else-if="setBrand === 'xbox'">
+        <template v-slot:image
+          ><base-achievement-image></base-achievement-image
+        ></template>
+        <template v-slot:text>
+          <div class="text-wrapper" :class="setBrand">
+            <base-achievement-header></base-achievement-header>
+            <span class="desc">
+              <base-achievement-text></base-achievement-text
+            ></span></div></template
+      ></xbox-card>
+
+      <playstation-card v-else-if="setBrand === 'playstation'">
+        <template v-slot:image
+          ><base-achievement-image shape="square"></base-achievement-image
+        ></template>
+        <template v-slot:text>
+          <div class="text-wrapper" :class="setBrand">
+            <base-achievement-header></base-achievement-header
+            ><base-achievement-text></base-achievement-text></div
+        >
+        <span class="ps-logo">
+          <font-awesome-icon :icon="['fab', 'playstation']"></font-awesome-icon>
+        </span>
+        </template>
+      </playstation-card>
+
+    </base-achievement>
+    <button @click="saveImage" v-if="filled">
+      <font-awesome-icon
+        :icon="['fad', 'cloud-download-alt']"
+      ></font-awesome-icon>
+      <span>Download Image File</span>
+    </button>
+  </section>
+</template>
+<script>
+import * as htmlToImage from "html-to-image";
+import FileSaver from "file-saver";
+import { computed } from "vue";
+import { useStore } from "vuex";
+import NintendoCard from "../achievement-cards/NintendoCard";
+import MinnmaxCard from "../achievement-cards/MinnmaxCard";
+import XboxCard from "../achievement-cards/XboxCard";
+import PlaystationCard from "../achievement-cards/PlaystationCard";
+export default {
+  components: { NintendoCard, MinnmaxCard, XboxCard, PlaystationCard },
+  setup() {
+    const store = useStore();
+    const saveImage = () => {
+      htmlToImage
+        .toBlob(document.getElementById("achievement"))
+        .then(function(blob) {
+          FileSaver.saveAs(blob, "achievement.png");
+        });
+    };
+    const setBrand = computed(() => {
+      return store.getters.getBrand;
+    });
+
+    const filled = computed(() => {
+      return store.getters.getHeader !== "" && store.getters.getText !== "";
+    });
+
+    return { saveImage, filled, setBrand };
+  },
+};
+</script>
+<style lang="scss" scoped>
+section {
+  @include flexbox();
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: auto;
+  width: 670px;
+
+  .text-wrapper {
+    @include flexbox();
+    flex-wrap: wrap;
+    align-content: flex-start;
+    align-items: center;
+    justify-content: flex-start;
+    margin-left: 10px;
+  }
+
+  .text-wrapper.minnmax {
+    width: 380px;
+  }
+
+  .text-wrapper.nintendo {
+    width: 430px;
+  }
+
+  .text-wrapper.xbox {
+    width: calc(100% - 150px);
+
+    .desc {
+      @include flexbox();
+      justify-content: flex-start;
+      align-items: center;
+    }
+  }
+
+  .text-wrapper.playstation {
+    width: 530px;
+  }
+
+  .ps-logo {
+    border-radius: 50%;
+    @include flexbox();
+    justify-content: center;
+    align-items: center;
+    background: #ffffff;
+    padding: 4px;
+
+    svg {
+      color: $ps-black;
+      font-size: 1.5em;
+    }
+  }
+
+  button {
+    background: none;
+    border: 3px solid #333333;
+    border-radius: 4em;
+    color: #333333;
+    @include flexbox();
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    margin: 3em auto 1em;
+    padding: 1.25em;
+    transition: all 0.3s ease;
+
+    svg {
+      font-size: 2em;
+      margin-right: 0.5em;
+    }
+
+    span {
+      font-size: 1.5em;
+      font-weight: 500;
+    }
+  }
+
+  button:hover {
+    border: 3px solid #16c172;
+    color: #16c172;
+    span {
+      font-weight: 500;
+    }
+  }
+}
+</style>
